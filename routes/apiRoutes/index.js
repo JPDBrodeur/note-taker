9c2cd21994594c6b2b9af59f0826fa6ddd4292b5
JPDBrodeur <jbrodeur001@gmail.com>
2021-10-09 19:29:14 -0400

    DRY & updated delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,58 +1,26 @@
 const router = require('express').Router();
-const createNewNote = require('../../lib/notes');
-const notes = require('../../db/db.json');
+const { createNewNote, readNotes, deleteNote } = require('../../lib/notes');
 const uniqid = require('uniqid');
 
-const fs = require('fs');
-const path = require('path');
-
 router.get('/notes', (req, res) => {
-    res.json(notes);
+    res.json(readNotes());
 });
 
 router.post('/notes', (req, res) => {
-    // set id with npm package uniqid
-    req.body.id = uniqid();
-
     // if the note does not contain a title, send 400 error back
     if (!req.body.title) {
         res.status(400).send('The note was not given a title.');
     } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
+        // set id with npm package uniqid
+        req.body.id = uniqid();
+        createNewNote(req.body);
+        res.json(req.body);
     }
 });
 
 router.delete('/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    fs.readFile(
-        path.join(__dirname, '../../db/db.json'),
-        (err, data) => {
-            if (err) {
-                console.log(err);
-                res.sendStatus(500);
-                return;
-            }
-            const json = JSON.parse(data);
-
-            for (let i = 0; i < json.length; i++) {
-                if (json[i].id === noteId) {
-                    json.splice(i, 1);
-                    return;
-                }
-            }
-            fs.writeFile(
-                path.join(__dirname, '../../db/db.json'),
-                JSON.stringify(json), 
-                (err) => {
-                    if(err) {
-                        console.log(err);
-                        res.sendStatus(500);
-                        return;
-                    }
-                    res.send('Successfully deleted');
-                });
-        }); 
+    const updatedNotes = deleteNote(req.params.id);
+    res.json(updatedNotes);
 });
 
 module.exports = router;
